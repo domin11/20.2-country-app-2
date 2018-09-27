64896268dac8ac8dea1dd8b4129616cc481ed8e6
Dominik Soboń <dominik.sobon@gmail.com>
2018-09-27 18:13:59 +0200

    Add js components and style

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
-import store from './store';
+import store from './store/';
 import DevTools from './DevTools';
-import { getCountries } from './actions/actions-countries';
+import routes from './routes';
+import './country.css';
 
 render(
   <Provider store={store}>
     <div>
       <DevTools />
-      <h1>Inicjalizacja projektu</h1>
+      <Router history={hashHistory} routes={routes} />
     </div>
   </Provider>,
   document.getElementById('root')
 );
-
-store.dispatch(getCountries());
